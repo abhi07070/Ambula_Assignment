2412ffa110c1490fda04effb100fdfd654c04e31
Davinder Kumar <abhiverma97819@gmail.com>
2023-07-06 21:29:08 +0530

    changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
     return (
         <header>
             <div className='logo'>
-                <Link to='/'><h1>Assignment</h1></Link>
+                <Link to='/Ambula_Assignment/'><h1>Assignment</h1></Link>
             </div>
             <div className="links">
                 <Link to="/">Home</Link>
